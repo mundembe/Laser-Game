be80e5646bcf62e921b260546c52360dc7ae9880
mundembe <61733352+mundembe@users.noreply.github.com>
2022-02-17 09:26:57 +0200

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,102 @@
+package com.main;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable{
+	
+	private static final long serialVersionUID = -4110424383919632746L;
+	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+
+	public static void main(String args[]) {
+		
+	new Game();
+		
+	}
+
+	private Thread thread;
+	private boolean running = false;
+
+	public synchronized void start() {
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+		
+	}
+	
+	public synchronized void stop() {
+
+		try {
+			thread.join();
+			running = false;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void run() {
+
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			
+			while(delta >= 1) {
+				tick();
+				delta--;
+			}
+			if (running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		
+		stop();
+		
+	}
+
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		g.dispose();
+		bs.show();
+		
+	}
+
+	private void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public Game() {
+		
+		new Window(WIDTH, HEIGHT, "Laser Game", this);
+		
+	}
+
+}
+	
